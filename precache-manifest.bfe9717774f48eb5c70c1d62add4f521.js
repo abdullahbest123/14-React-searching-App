c953c12e4997e454c29e9e19bea582c6c4215f5f
abdullahbest123 <abdullahbest19@gmail.com>
2023-03-28 20:50:33 +0500

    Updates

---
+++ precache-manifest.bfe9717774f48eb5c70c1d62add4f521.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55a1ee3de5062b3b24ba05ad07c6ee86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c6c8abea7fe8b3bdfa0",
+    "url": "/static/css/main.0a967299.chunk.css"
+  },
+  {
+    "revision": "a44faac5f947f646d8c6",
+    "url": "/static/js/2.d3f5d21a.chunk.js"
+  },
+  {
+    "revision": "afdf66fdeeebc7ce3b76964dbba36c87",
+    "url": "/static/js/2.d3f5d21a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6c8abea7fe8b3bdfa0",
+    "url": "/static/js/main.9eb2dade.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.9eb2dade.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d8ea8e0de0fd66811a",
+    "url": "/static/js/runtime-main.bb80f66d.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/static/media/logo.9a3d2645.svg"
+  }
+]);
